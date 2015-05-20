4b5ec2517754544d886b093b335f2694f84becdf
Akram Rameez <rameez2612@gmail.com>
2015-05-20 15:46:40 +0530

    Almost there
    Its working for the last permutation. Have to refine the callback

--- 024/24 story.py
+++ 024/24 story.py
@@ -1,75 +1,100 @@
 # A permutation is an ordered arrangement of objects. For example, 3124 is one possible permutation of the digits 1, 2, 3 and 4. If all of the permutations are listed numerically or alphabetically, we call it lexicographic order. The lexicographic permutations of 0, 1 and 2 are:
-# 012   021   102   120   201   210
+# 012   021   102   120  201   210
 # What is the millionth lexicographic permutation of the digits 0, 1, 2, 3, 4, 5, 6, 7, 8 and 9?
 
-# 0 123
-# 0 132
-# 0 213
-# 0 231
-# 0 312
-# 0 321
-
-# 1023
-# 1032
-# 1203
-# 1230
-# 1302
-# 1320
-# 2013
-# 2
-# 2
-# 2
-# 2
-# 2
-# 3
-# 3
-# 3
-# 3
-# 3
-# 3
+# 1  0123
+# 2  0132
+# 3  0213
+# 4  0231
+# 5  0312
+# 6  0321
+# 7  1023
+# 8  1032
+# 9  1203
+# 10 1230
+# 11 1302
+# 12 1320
+# 13 2013
+# 14 2031
+# 15 2103
+# 16 2130
+# 17 2301
+# 18 2310
+# 19 3012
+# 20 3021
+# 21 3102
+# 22 3120
+# 23 3201
+# 24 3210
 
 import math
 
 digits = [0,1,2,3]
 digits.sort()
-print "Number of digits:",len(digits)
-print "digits:",digits
+# print "Number of digits:",len(digits)
+# print "digits:",digits
 
-n = 5
+n = 10
+n = n-1
 
-print "digits change in the place every",math.factorial(len(digits)-1),"numbers"
-print "there are a total of",math.factorial(len(digits)),"permutations"
+# print "digits change in the place every",math.factorial(len(digits)-1),"numbers"
+# print "there are a total of",math.factorial(len(digits)),"permutations"
 
-i,j,k=0,n-math.factorial(len(digits)-1),math.factorial(len(digits)-1)
-print i,j,k
+i = int(n/math.factorial(len(digits)-1))
+# print i
+# print "first digit is",digits[i]
 
-while(j>=0):
-	i=i+1
-	j=j-k
+print digits[i],
 
-print "first digit is",digits[i]
-
-del digits[i]
-
-print "numbers remaining:",digits
-
-n=n-(math.factorial(len(digits))*(i+1))
-print n
-
-print "digits change in the place every",math.factorial(len(digits)-1),"numbers"
-print "there are a total of",math.factorial(len(digits)),"permutations"
-
-i,j,k=0,n-math.factorial(len(digits)-1),math.factorial(len(digits)-1)
-print i,j,k
-
-while(j>0):
-	i=i+1
-	j=j-k
-
-print i
-print "second digit is",digits[i]
 
 del digits[i]
 
-print "numbers remaining:",digits
+# print "numbers remaining:",digits
 
+n=n%(i*math.factorial(len(digits)-1))
+# print n
+
+# print "digits change in the place every",math.factorial(len(digits)-1),"numbers"
+# print "there are a total of",math.factorial(len(digits)),"permutations"
+
+i = int(n/math.factorial(len(digits)-1))
+# print i,n
+# print "second digit is",digits[i]
+
+print digits[i],
+del digits[i]
+
+# print "numbers remaining:",digits
+
+n=n%(i*math.factorial(len(digits)-1))
+# print n
+
+# print "digits change in the place every",math.factorial(len(digits)-1),"numbers"
+# print "there are a total of",math.factorial(len(digits)),"permutations"
+
+i = int(n/math.factorial(len(digits)-1))
+# print i,n
+# print "third digit is",digits[i]
+
+print digits[i],
+del digits[i]
+
+# print "numbers remaining:",digits
+
+n=n%(i*math.factorial(len(digits)-1))
+
+# n=n%(i*math.factorial(len(digits)-1))
+# print n
+
+# print "digits change in the place every",math.factorial(len(digits)-1),"numbers"
+# print "there are a total of",math.factorial(len(digits)),"permutations"
+
+i = int(n/math.factorial(len(digits)-1))
+# print i,n
+# print "fourth digit is",digits[i]
+
+print digits[i],
+
+del digits[i]
+
+# print "numbers remaining:",digits
--- 024/24.py
+++ 024/24.py
@@ -4,15 +4,42 @@
 
 import math
 
-digits = [0,1,2]
+permutations=["0123","0132","0213","0231","0312","0321","1023","1032","1203","1230","1302","1320","2013","2031","2103","2130","2301","2310","3012","3021","3102","3120","3201","3210"]
+digits = ['0','1','2','3']
+digits.sort()
 ans=[]
-n=4
+n=23
+rans=n
+print "Number of digits:",len(digits)
+print "digits:",digits
 
-i,j,k=0,n-math.factorial(len(digits)-1),math.factorial(len(digits)-1)
+print "we need the",n+1,"permutation"
 
-while(len(digits)>1):
-	while(j>=0):
-		i=i+1
-		j=j-k
+ctr=1
+
+while(len(digits)>0):
+
+	print "--------------------"
+
+	print "digits change in the place every",math.factorial(len(digits)-1),"numbers"
+	print "there are a total of",math.factorial(len(digits)),"permutations"
+
+	i = int(n/math.factorial(len(digits)-1))
+	print "i before int:",n*1.0/math.factorial(len(digits)-1)
+
+	# ans=10*ans+digits[i]
+	print ctr,"digit is",digits[i]
 	ans.append(digits[i])
 	del digits[i]
+	print digits,"-----"
+	if i==0:
+		for x in digits:
+			ans.append(x)
+			# ans=10*ans+digits[i]
+		break
+
+	n=n%(i*math.factorial(len(digits)-1))
+
+print "--------------------"
+print "answer obtained :",''.join(ans)
+print "actual answer   :",permutations[rans]
